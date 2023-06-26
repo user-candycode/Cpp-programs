63d8e42ad552beedfa77dbaf7d6fe8d885f90891
candy-code <user-candycode@github.com>
2023-06-26 19:25:49 +0530

    Second commit

---
+++ restart/basics/data_type_conversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 'a';
+  cout<<"a: "<<a<<endl;
+
+  char ch = 98;
+  cout<<"ch: "<<ch<<endl;
+
+  char ch1 = 123456;
+  cout<<"ch1: "<<ch1<<endl;
+
+  char ch2 = 512;
+  cout<<"ch2: "<<ch2<<endl;
+char ch3;
+  for(int i = 0; i<=512; i++){
+    ch3 = i;
+    cout<<i<<" = "<<ch3<<endl;
+  }
+return 0;
+}
