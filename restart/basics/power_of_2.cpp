4327a4320262a1e4db84105c43767a32d1ccb817
user-candycode <user-candycode@github.com>
2023-07-07 21:06:19 +0530

    starting searching and 2d arrray

--- restart/basics/power_of_2.cpp
+++ restart/basics/power_of_2.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-// #include<cmath>
+#include<cmath>
 using namespace std;
 
 void method1(int n){
@@ -24,11 +24,71 @@ void method1(int n){
 
 
 void method2(int n){
+    // Initilizing a variable x with 1 and Leftshifting the x until x==n also if temp == x i.e  x&n gives back again x,
+    // means its not match so inc count.
     //16 0001 0000
+    //15 0000 1111
+    //21 0001 0101
+    int temp;
+    int x = 1;
+    int counter = 0;
+    
+    while (x<=n)
+    {
+        temp = n & x;
+        if (temp==x)
+        {
+            counter++;
+        }
+        x = x<<1;
+    }
+    cout<<"counter: "<<counter<<endl;
+}
+
+void method3(int n){
+    // 16 0001 0000
+    // 01 0000 0001
+    
+    int x = n;
+    while (x>1 && (n&1)==0)
+    {
+        x=x>>1;
+    }
+    
+    if(n==1 || n!=0){
+        cout<<"is in power of 2"<<endl;
+    }else{
+        cout<<"is not in power of 2"<<endl;
+    }
+    cout<<"does not work got bored"<<endl;
+}
+
+void method4(int n){
+    bool temp = floor(log2(n))==ceil(log2(n)) ? true:false;
+    if(temp && n!=0){
+        cout<<"is in power of 2"<<endl;
+    }else{
+        cout<<"is not in power of 2"<<endl;
+    }
+
 }
 
 int main(){
-    // method1(1);
-    method2(16);
+    int num;
+    // num=16;
+    num=1;
+    // num=0;
+    // num=8;
+    // num=15;
+    // num=21;
+
+
+    // method1(num);
+    // method2(num);
+    // method3(num);
+
+    method4(num);
+  
+
     return 0;
 }
