33e3d15f8dcc7722d9d504edf228f7b12450819d
user-candycode <user.candycode@gmail.com>
2023-07-21 21:25:25 +0530

    square root the decimal part using bin search

---
+++ restart/practice/squareRootOfIntergerWithFloatingPrecision.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int square_root(int n){
+    int ans=1;
+    int low = 1;
+    int high = n/2 + 1;
+    while(low<high){
+        int mid = high + (low-high)/2;
+        if((mid*mid)<=n){
+            low = mid;
+            ans = low;
+        }else{
+            high = mid - 1;
+        }
+        
+    }
+    return ans;
+}
+
+int main(){
+    int num;
+    num = 9;
+
+    cout<<square_root(num)<<endl;
+    return 0;
+}
