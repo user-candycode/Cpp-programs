4fa023be98af93869c89a9a84f859aba03ff4731
user-candycode <user.candycode@gmail.com>
2023-07-25 11:17:22 +0530

    selection sort

---
+++ restart/sorting/selection_sort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr={29,72,98,13,13,87,66,52,51,36};
+    // printing vector array
+    cout<<"before:\t";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+
+    int i=0;
+    while (i<arr.size())
+    {
+        int small = i;
+        int j=i+1;
+        while (j<arr.size())
+        {
+            if(arr[small]>arr[j]){
+                small = j;
+            }
+            j++;
+        }
+        swap(arr[small],arr[i]);
+        i++;
+    }
+    
+    // printing vector array
+    cout<<"after:\t";
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<",";
+    }
+    
+    return 0;
+    // O(n^2) small memory footprint
+}
