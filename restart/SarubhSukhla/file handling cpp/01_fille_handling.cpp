8221c4947346ff56fd4aa59b82d4e24f5e79caa5
user-candycode <user.candycode@gmail.com>
2023-08-16 22:20:34 +0530

    Outputing to a file on secondary device

---
+++ restart/SarubhSukhla/file handling cpp/01_fille_handling.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ofstream fout;
+    fout.open("myfile.dat");
+    fout<<"hello";
+    fout.close();
+    return 0;
+}
+
+
+/*
+
+       |->ostream-----|----->ofstream
+       |       |      |----->ostream_withoutassign(cout)
+       |       |
+ios ---|       |->iostream->fstream
+       |       |
+       |       |      |---->istream_withoutassign(cin)
+       |->istream-----|---->ifstream
+
+*/
